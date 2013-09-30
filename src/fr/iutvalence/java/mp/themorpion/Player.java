0a99a3f6375d4dbaa08415f21081283b9d2f400e
sebastienjean <baz.jean@gmail.com>
2013-09-30 19:33:46 +0200

    Reviewed code, added some TODOs

--- src/fr/iutvalence/java/mp/themorpion/Player.java
+++ src/fr/iutvalence/java/mp/themorpion/Player.java
@@ -1,46 +1,52 @@
 package fr.iutvalence.java.mp.themorpion;
+
 /**
  * 
  * This class describes a player with his attributes
+ * 
  * @author andrvale
- *
+ * 
  */
 
 public class Player
 {
-  
-  /**
- * Identifiant of the player (value = 1 or 2)
- */
-private int id;
-  /**
- * Score of the player
- */
-private int score;
-  
+    // TODO (think about it) this field seems useless
+    /**
+     * Identifier of the player (value = 1 or 2)
+     */
+    private int id;
+    
+    /**
+     * Score of the player
+     */
+    private int score;
 
-/**
- * pion is the attribute which represent the cross or the circle 
- * of the TTT game 
- * 
- */
-private int pion;
+    // TODO (think about it) this field seems useless
+    /**
+     * pion is the attribute which represent the cross or the circle of the TTT
+     * game
+     * 
+     */
+    // TODO (
+    // TODO (fix) rename field (mark)
+    // TODO (fix) document what values are allowed and what they mean
+    private int pion;
 
+    // TODO (fix) simplify comment, describe what the player looks like once created
+    // TODO (fix) document parameters
+    /**
+     * This is the constructor of the class Player which takes 2 parameters :
+     * the id of the player and the shape of his token
+     * 
+     * @param id
+     * @param score
+     * @param pion
+     */
 
-/**
- * This is the constructor of the class Player which takes 2 parameters : 
- * the id of the player and the shape of his token
- * 
- * @param id
- * @param score
- * @param pion
- */
-
-Player(int id, int pion)
-{
-    this.id = id;
-    this.score = 0;
-    this.pion = pion;
-} 
- 
+    Player(int id, int pion)
+    {
+        this.id = id;
+        this.score = 0;
+        this.pion = pion;
+    }
 }
--- src/fr/iutvalence/java/mp/themorpion/TicTacToe.java
+++ src/fr/iutvalence/java/mp/themorpion/TicTacToe.java
@@ -1,45 +1,57 @@
 package fr.iutvalence.java.mp.themorpion;
+
 /**
  * 
  * This class represents a Tic-Tac-Toe game
+ * 
  * @author andrvale
- *
+ * 
  */
 
 public class TicTacToe
 {
+    // TODO (fix) document what values are allowed and what they mean
     /**
      * This is the representation of the grid of Tic Tac Toe
+     * 
      */
     int grid[][];
-    
+
+    // TODO (fix) simplify comment
     /**
      * Declaration of the first player
      */
-    Player player_one;   
-    
+    // TODO (fix) comply with naming conventions
+    Player player_one;
+
+    // TODO (fix) simplify comment
     /**
      * Declaration of the second player
      */
+    // TODO (fix) comply with naming conventions
     Player player_two;
 
+    // TODO (fix) simplify comment
+    // TODO (fix) document parameters
+    // TODO (fix) declare a visibility for the constructor
     /**
-     * Constructor of Tic Tac Toe class
-     * It creates a new grid 3x3 and 2 new players
+     * Constructor of Tic Tac Toe class It creates a new grid 3x3 and 2 new
+     * players
+     * 
      * @param grid
      * @param player_one
      * @param player_two
      */
-    
+
     TicTacToe()
     {
-        final int  CIRCLE = 10000;
-        final int  CROSS = 20000;
-        
+        // TODO (fix) define constants for that
+        final int CIRCLE = 10000;
+        final int CROSS = 20000;
+
         this.grid = new int[3][3];
         this.player_one = new Player(1, CIRCLE);
         this.player_two = new Player(2, CROSS);
     }
-       
-    
+
 }
