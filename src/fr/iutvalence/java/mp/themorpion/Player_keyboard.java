4865dd496aa4193cca3a4489986808a42ef2e7ac
andrvale <andrvale@pc-b122-04.iut-valence.fr>
2013-11-21 12:31:37 +0100

    Delete method to associate numeric keyboard

--- src/fr/iutvalence/java/mp/themorpion/Player_keyboard.java
+++ src/fr/iutvalence/java/mp/themorpion/Player_keyboard.java
@@ -8,71 +8,35 @@ import java.util.Scanner;
  */
 public class Player_keyboard implements Player
 {
+    public final static Position[] positionsFromCellNumber = new Position[] 
+            { new Position(2,0), new Position(2,1), new Position(2,2),
+              new Position(1,0), new Position(1,1), new Position(1,2),
+              new Position(0,0), new Position(0,1), new Position(0,2)
+ 
+            };
     /**
      * Ask a position to the player on the numeric keyboard
      * 
      * @return position wanted by the player
      * 
      */    
+    /* (non-Javadoc)
+     * @see fr.iutvalence.java.mp.themorpion.Player#askPosition()
+     */
     public Position askPosition()
     {
         // int column, line;
         Position pos;
         Scanner sc = new Scanner(System.in);
         System.out.println("Veuillez saisir votre position :");
-        int p = sc.nextInt();
+        int cellNumber = sc.nextInt();
         /*line = p /10;
         column = p%10;
         pos = new Position(line, column);*/
-        pos = askToKeyboard(p);
-        return pos;
+        if ((cellNumber > 9) ||(cellNumber < 1 )) return new Position(3,3);
+        return positionsFromCellNumber[cellNumber-1];
 
     }
 
-    /**
-     * Numeric keyboard correspond to the grid of TicTacToe : 1 is (2,0),etc
-     * @param p number on numeric keyboard
-     * @return position 
-     */
-    public Position askToKeyboard(int p)
-    {
-        if (p == 1)
-        {
-            return (new Position(2,0));
-        }
-        if (p == 2)
-        {
-            return (new Position(2,1));
-        }
-        if (p == 3)
-        {
-            return (new Position(2,2));
-        }
-        if (p == 4)
-        {
-            return (new Position(1,0));
-        }
-        if (p == 5)
-        {
-            return (new Position(1,1));
-        }
-        if (p == 6)
-        {
-            return (new Position(1,2));
-        }
-        if (p == 7)
-        {
-            return (new Position(0,0));
-        }
-        if (p == 8)
-        {
-            return (new Position(0,1));
-        }
-        if (p == 9)
-        {
-            return (new Position(0,2));
-        }
-        return null;
-    }
   }
 
